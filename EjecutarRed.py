b41376dab4d688a1f6c78a1f79309ef792d98db2
RubenCA <ruben1000.ca@gmail.com>
2024-11-21 20:39:01 -0600

    Cambios en el script principal de ejecución

--- EjecutarRed.py
+++ EjecutarRed.py
@@ -1,52 +1,54 @@
 from lib.Utilerias import *
 from modelo.RedMonocapa import RedNeuronalMonocapa
 
-datos = cargarArchivoCSV("datos/Monocapa.csv", removerEncabezado = True)
-
-dataset = matrizEnteros(datos)
-
-datasetEntrenamiento, datasetPrueba = dividirDatos(dataset, 0.75)
-
-print("\nNúmero de registros del dataset:", len(dataset))
-
-print("Número de registros de entrenamiento:", len(datasetEntrenamiento))
-
-print("Número de registros de prueba:", len(datasetPrueba), end="\n"*2)
-
-entradasEntrenamiento, salidasEntrenamiento = dividirMatriz(datasetEntrenamiento)
-
-salidasEntrenamiento = matrizTranspuesta(salidasEntrenamiento)
-
-entradasPrueba, salidasEsperadas = dividirMatriz(datasetPrueba)
-
-
-
-entradasEntrenamiento = normalizar(entradasEntrenamiento)
-
-entradasPrueba = normalizar(entradasPrueba)
-
-
-
-red = RedNeuronalMonocapa(3, 3, 0.2)
-
-red.entrenar(entradasEntrenamiento, salidasEntrenamiento, 100)
-
-casosCorrectos, casosIncorrectos, precisionRed = calcularRendimientoRed(red, entradasPrueba, salidasEsperadas)
-
-print("Numero de casos correctos:", len(casosCorrectos))
-
-print("Numero de casos incorrectos:", len(casosIncorrectos))
-
-print(f"Precisión de la red: {precisionRed * 100} %")
-
-print("\nCasos de prueba correctos\n")
-
-imprimirCasos(casosCorrectos)
-
-print("\nCasos de prueba incorrectos\n")
-
-imprimirCasos(casosIncorrectos)
-
-print("\nInformacion de la red\n")
-
-red.imprimirInformacion()
+if __name__ == "__main__":
+	
+	datos = cargarArchivoCSV("datos/Monocapa.csv", removerEncabezado = True)
+	
+	dataset = matrizEnteros(datos)
+	
+	datasetEntrenamiento, datasetPrueba = dividirDatos(dataset, 0.75)
+	
+	print("\nNúmero de registros del dataset:", len(dataset))
+	
+	print("Número de registros de entrenamiento:", len(datasetEntrenamiento))
+	
+	print("Número de registros de prueba:", len(datasetPrueba), end="\n"*2)
+	
+	entradasEntrenamiento, salidasEntrenamiento = dividirMatriz(datasetEntrenamiento)
+	
+	salidasEntrenamiento = matrizTranspuesta(salidasEntrenamiento)
+	
+	entradasPrueba, salidasEsperadas = dividirMatriz(datasetPrueba)
+	
+	
+	
+	entradasEntrenamiento = normalizar(entradasEntrenamiento)
+	
+	entradasPrueba = normalizar(entradasPrueba)
+	
+	
+	
+	red = RedNeuronalMonocapa(3, 3, 0.2)
+	
+	red.entrenar(entradasEntrenamiento, salidasEntrenamiento, 100)
+	
+	casosCorrectos, casosIncorrectos, precisionRed = calcularRendimientoRed(red, entradasPrueba, salidasEsperadas)
+	
+	print("Numero de casos correctos:", len(casosCorrectos))
+	
+	print("Numero de casos incorrectos:", len(casosIncorrectos))
+	
+	print(f"Precisión de la red: {precisionRed * 100} %")
+	
+	print("\nCasos de prueba correctos\n")
+	
+	imprimirCasos(casosCorrectos)
+	
+	print("\nCasos de prueba incorrectos\n")
+	
+	imprimirCasos(casosIncorrectos)
+	
+	print("\nInformacion de la red\n")
+	
+	red.imprimirInformacion()
