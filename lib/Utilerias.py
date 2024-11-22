4e7885228b4c2a1423c3343c6a43663dec23377f
RubenCA <ruben1000.ca@gmail.com>
2024-11-21 19:26:27 -0600

    Creacion de nuevas funciones y corrección de errores

--- lib/Utilerias.py
+++ lib/Utilerias.py
@@ -1,6 +1,8 @@
 """Modulo que contiene la definicion de algunas funciones de ayuda para poder tratar
 los datos que seran utilizados por la red."""
 
+import random
+
 
 def cargarArchivoCSV(rutaArchivo, separador = ",", removerEncabezado = False):
 
@@ -21,7 +23,7 @@ def imprimirMatriz(matriz):
 
         print(fila)
 
-def convertirMatrizAEnteros(matriz):
+def matrizEnteros(matriz):
 
     """Esta funcion convierte una matriz que contiene valores numericos
     representados como cadenas, a su matriz equivalente pero ahora
@@ -39,7 +41,7 @@ def encontrarMinimosMaximos(matriz):
 
     numeroColumnas = len(matriz[0])
 
-    valores = [{"minimo" : matriz[-1][columna] , "maximo" : [0][columna]} for columna in range(numeroColumnas)]
+    valores = [{"minimo" : matriz[-1][columna] , "maximo" : matriz[0][columna]} for columna in range(numeroColumnas)]
 
     for fila in range(numeroFilas):
 
@@ -63,9 +65,148 @@ def encontrarMinimosMaximos(matriz):
 def normalizar(dataset):
 
     """Esta funcion permite aplicar una normalizacion min-max de 0 a 1 para cada uno de
-    los diferentes valores de las distintas columnas que integran el dataset"""
+    los diferentes valores de las distintas columnas que integran un dataset expresado como matriz"""
+
+    datasetNormalizado = []
+
+    informacionColumnas = encontrarMinimosMaximos(dataset)
+
+    for fila in range(len(dataset)):
+
+        filaNormalizada = []
+
+        for columna in range(len(dataset[0])):
+
+            valor = dataset[fila][columna]
+
+            datosColumna = informacionColumnas[columna]
+
+            filaNormalizada.append((valor - datosColumna["minimo"]) / (datosColumna["maximo"] - datosColumna["minimo"]))
+
+        datasetNormalizado.append(filaNormalizada)
+
+    return datasetNormalizado
 
 
+def matrizTranspuesta(matriz):
+
+    transpuesta = [[] for columna in range(len(matriz[0]))]
+
+    for fila in matriz:
+
+        for indiceColumna in range(len(matriz[0])):
+
+            transpuesta[indiceColumna].append(fila[indiceColumna])
+
+
+    return transpuesta
+
+
+def dividirMatriz(matriz):
+
+    """Esta funcion tiene el proposito de partir una matriz exactamente
+    por la mitad. Por ello, el número de columnas de la matriz debe de ser
+    un número par."""
+
+    numeroColumnas = len(matriz[0])
+
+    if numeroColumnas % 2 != 0:
+
+        raise ValueError("Esta matriz no tiene un número par de columnas")
+
+    submatrices = ([],[])
+
+    columnaLimite = numeroColumnas // 2
+
+    for fila in matriz:
+
+        submatrices[0].append(fila[:columnaLimite])
+
+        submatrices[1].append(fila[columnaLimite:])
+
+    return submatrices
+
+
+def dividirDatos(dataset, ratio):
+
+    """Esta función tiene la finalidad de dividir el dataset pasado como
+    argumento, en datos para entrenamiento y datos para prueba.
+    
+    Esta función recibe como argumentos el dataset del que se quieren dividir
+    los datos, así como el ratio (un valor entre 0.0 y 1.0) que indique
+    el porcentaje de datos que se quieren para entrenamiento.
+
+    Esta función devuelve una tupla, donde el primer elemento contiene la
+    porcion del dataset, delimitado por el ratio pasado por argumento, que
+    servira para realizar el entrenamiento de la red; mientras que como
+    segundo elemento de la tupla contiene el resto del dataset que debería
+    de ser utilizado para probar la red una vez que esté entrenada.
+    """
+
+    if not (0.0 <= ratio <= 1):
+
+        raise ValueError("El valor del ratio debe de ser entre 0.0 y 1.0")
+
+    datasetPrueba = dataset[:]
+
+    dimensionEntrenamiento = int(len(dataset) * ratio)
+
+    random.shuffle(datasetPrueba)
+
+    datasetEntrenamiento = []
+
+    while len(datasetEntrenamiento) < dimensionEntrenamiento:
+
+        datasetEntrenamiento.append(datasetPrueba.pop())
+
+    return (datasetEntrenamiento, datasetPrueba) 
+
+
+def calcularRendimientoRed(redNeuronalMonocapa, matrizEntradas, matrizSalidasEsperadas):
+
+    if len(matrizEntradas) != len(matrizSalidasEsperadas):
+
+        raise ValueError("La matriz de entradas y la matriz de salidas de salidas esperadas deben de tener el mismo número de filas")
+
+    casosCorrectos = []
+
+    casosIncorrectos = []
+   
+    for indiceCaso in range(len(matrizEntradas)):
+
+        vectorEntradas = matrizEntradas[indiceCaso]
+
+        salidasEsperadas = matrizSalidasEsperadas[indiceCaso]
+
+        salidasRed = redNeuronalMonocapa.probar(vectorEntradas)
+
+        registro = (vectorEntradas, salidasEsperadas, salidasRed)
+
+        if salidasRed == salidasEsperadas:
+
+            casosCorrectos.append(registro)
+        else:
+
+            casosIncorrectos.append(registro)
+
+    precision = len(casosCorrectos) / len(matrizEntradas)
+
+    return (casosCorrectos, casosIncorrectos, precision)
+
+        
+def imprimirCasos(casos):
+
+    print("-"*55)
+    
+    print("{:15}|{:20}|{:20}".format("Entradas", "Salidas esperadas", "Salidas obtenidas"))
+
+    print("-"*55)
+
+    for registro in casos:
+
+        entradas, salidasEsperadas, salidasObtenidas = registro
+
+        print("{:15}|{:20}|{:20}".format(str(entradas), str(salidasEsperadas), str(salidasObtenidas)))
 
 
 
